96b90e951b700deab3231e03050b43d998c89a47
iamgyu <iamgyu@naver.com>
2023-08-01 14:46:17 +0900

    reset jwt

--- todo/src/main/java/com/example/todo/MemberController.java
+++ todo/src/main/java/com/example/todo/MemberController.java
@@ -8,20 +8,6 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final Member_Sqlite memberSqlite = new Member_Sqlite();
 
-    private final MemberService memberService;
-
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
-
-    @PostMapping("/login")
-    public TokenDto login(@RequestBody MemberDto memberDto){
-        String id = memberDto.getId();
-        String password = memberDto.getPassword();
-        TokenDto tokenDto = memberService.login(id, password);
-        return tokenDto;
-    }
-
     @PostMapping("")
     public void input(@RequestBody MemberDto memberDto){
         memberSqlite.inputMember(memberDto.getId(), memberDto.getPassword());
